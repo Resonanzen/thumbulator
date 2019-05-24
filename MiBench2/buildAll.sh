89b3d0681de4057f058da2492d2cd3beb6dd4362
charley <charleyxu2@gmail.com>
2019-05-23 22:41:24 -0400

    added benchmarks under MiBench2 folder. Makefile and buildAll.sh edited so most things should work. You may still need to edit the path in Makefile.mk to your installation of the arm-gcc 2015q4 toolchain.

---
+++ MiBench2/buildAll.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+find . -d  -type d \( ! -name . \) -exec bash -c "cd {} && make clean && make && cp main.bin ../{}.bin && make clean" \;
---
+++ MiBench2/myTest/main.c
@@ -0,0 +1,8 @@
+int main(void){
+
+
+	int hi = 5;	
+
+	return 0;
+
+}
